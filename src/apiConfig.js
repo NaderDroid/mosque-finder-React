8362ae6a661f0059a171b1dca9555c68b3217845
Nader <na511dr@hotmail.com>
2019-07-31 17:03:09 +0300

    updating the production url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const expressPort = 3003
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://mosque-finder.herokuapp.com',
   development: `http://localhost:${expressPort}`
 }
 
